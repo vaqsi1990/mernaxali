af1ebe11adcca15d691d5ceb679aeaf1d3857fa9
vaqsi1990 <97800985+vaqsi1990@users.noreply.github.com>
2024-02-06 21:31:17 +0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [''https://mernapp-v40n.onrender.com'];
 
 app.use(cors({
   origin: allowedOrigins,
